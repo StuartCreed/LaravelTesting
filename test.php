0a8c060eb5f4188fd1543cfdec574788a8352855
Stuart Creed <stuart.a.creed@gmail.com>
2020-07-05 14:43:26 +0100

    Initial Lavarel setup with route testing

---
+++ test.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "TEST";
+?>
+
+<?php
+print  "<b>TEST2</b>"
+?>
+
+<?php
+for ($x = 0; $x <= 10; $x++) {
+  echo "The number is: $x <br>";
+}
+?>
+
+<?php
+foreach (array(1, 2, 3, 4, 5) as $v) {
+    echo "$v\n";
+}
+?>
+
+</body>
+</html>
